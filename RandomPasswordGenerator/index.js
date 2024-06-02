ab42d86376bf1a060e6256a344b8133cf9521ca6
Aryan Izhar <115881821+AryanIzhar-Dev7@users.noreply.github.com>
2024-06-02 15:12:11 +0500

    RandomPasswordGenerator
    Simple Password Generator

---
+++ RandomPasswordGenerator/index.js
@@ -0,0 +1,42 @@
+const characters = ["A","B","C","D","E","F","G","H","I",
+"J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z","a","b","c","d","e",
+"f","g","h","i","j","k","l","m","n","o","p","q",
+"r","s","t","u","v","w","x","y","z", "0", "1", "2",
+ "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+ "$","%","^","&","*","(",")","_","-","+","=","{","[","}",
+ "]",",","|",":",";","<",">",".","?",
+"/"];
+
+// Using above character the passwords will be made
+
+let password1EL= document.querySelector("#password1");
+let password2EL = document.querySelector("#password2");
+
+
+
+function generate(){
+  
+let pass1="";
+let pass2=""
+
+let p1=0;
+let p2=0;
+
+for (let i=0; i< 15; i++){
+    //generating random number
+    p1=Math.floor(Math.random()*characters.length);
+    p2=Math.floor(Math.random()*characters.length);
+    
+    //making two different password pass1 and pass2
+    pass1+=characters[p1];
+    pass2+=characters[p2];
+}
+
+
+console.log(pass1);
+console.log(pass2);
+password1EL.value=pass1;
+password2EL.value=pass2;
+
+}
